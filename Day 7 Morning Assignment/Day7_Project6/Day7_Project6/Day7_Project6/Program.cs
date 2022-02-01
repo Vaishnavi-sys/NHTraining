2961a9b318602e623bc2b622b54c77fdbb885804
Vaishnavi-sys <vaishnavi.nanam@gmail.com>
2022-02-01 18:54:15 +0530

    Day 7 Submission

--- Day 7 Morning Assignment/Day7_Project6/Day7_Project6/Day7_Project6/Program.cs
+++ Day 7 Morning Assignment/Day7_Project6/Day7_Project6/Day7_Project6/Program.cs
@@ -10,42 +10,50 @@ namespace Day7_Project6
     {
         public int id;
         public string name;
-        public string email;
+        public int points;
     }
-      
     internal class Program
     {
         static void Main(string[] args)
         {
-            Customer[] customer = new Customer[6];
-            {
-                customer[0] = new Customer() { id = 1, name = "Vaishu", email = "vaish@gmail.com" };
-                customer[1] = new Customer() { id = 2, name = "Sindhu", email = "abcd@gmail.com" };
-                customer[2] = new Customer(){ id = 3, name = "Pavan", email = "pavan@gmail.com" };
-                customer[3] = new Customer() { id = 4, name = "Mouni", email = "Mouni25@gmail.com" };
-                customer[4] = new Customer() { id = 5, name = "swathi", email = "swathi_6@gmail.com" };
-            }
-            Console.WriteLine("=======================================================================================");
-            // for loop
-            for(int i=0;i<customer.Length;i++)
-            {
-                Console.WriteLine($"id={customer[i].id}, name={customer[i].name}, email={customer[i].email}");
+            Customer[] customer = new Customer[]
+                {
+                    new Customer() { id = 1, name ="john", points =95},
+                    new Customer() { id = 2, name ="prasad",points=25},
+                    new Customer() { id = 3, name ="sai",points=10},
+                    new Customer() { id = 4, name ="uday",points=64},
+                    new Customer() { id = 5, name ="jp",points=40}
+                };
 
-            }
+            Console.WriteLine("===========================================================");
 
-            Console.WriteLine("=======================================================================================");
-            // foreach loop
-            foreach (var c in customer)
+            //using for loop
+            for (int i = 0; i < customer.Length; i++)
             {
-                Console.WriteLine($"id={c.id}, name={c.name}, email={c.email}");
+                if (customer[i].points >= 30)
+                    Console.WriteLine($"id={customer[i].id},name={customer[i].name},Points={customer[i].points}");
             }
 
-            Console.WriteLine("=======================================================================================");
-            // Lambda Expression
-            customer.ToList().ToList().ForEach(c => Console.WriteLine($"id={c.id},name={c.name},salary{c.email}"));
+            Console.WriteLine("===========================================================");
+
+            //using foreach
+            foreach (var e in customer)
+            {
+                if (e.points >= 30)
+                    Console.WriteLine($"id={e.id},name={e.name},Points{e.points}");
+            }
+
+            Console.WriteLine("===========================================================");
+
+            //using lamda expression
+
+            customer.ToList().Where(e => e.points >= 30).ToList().ForEach(e => Console.WriteLine($"id={e.id},name={e.name},Points{e.points}"));
+
+            Console.WriteLine("===========================================================");
+
             Console.ReadLine();
         }
     }
-
 }
+
  
