498149eb10c224b2730b483227b4fb087a1a0b8c
Vaishnavi-sys <vaishnavi.nanam@gmail.com>
2022-02-10 23:46:29 +0530

    Day 14 Submission

--- Day 14 Assignment/Day14Project7/Day14Project7/Program.cs
+++ Day 14 Assignment/Day14Project7/Day14Project7/Program.cs
@@ -6,6 +6,19 @@ using System.Threading.Tasks;
 
 namespace Day14Project7
 {
+    interface IRules
+    {
+        int Age { get; set; }
+
+        int add(int a, int b);
+
+        public void PrintHi()
+        {
+            Console.WriteLine("Hi");
+        }
+    }
+
+
     internal class Program
     {
         static void Main(string[] args)
