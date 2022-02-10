13ccfd0ab6d78aea79702486735a35485c184096
Vaishnavi-sys <vaishnavi.nanam@gmail.com>
2022-02-10 20:00:24 +0530

    first number after 1000 which is

---
+++ Day 14 Assignment/Day14Project6/Day14Project6/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+// ********************************************************
+// Author : Nanam Vaishnavi
+// Purpose : 6. Find the first number after 1000 which is divisible by 97.
+//                HINT : use for loop and break
+// **********************************************************
+namespace Day14Project6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            for(int i=1000;i<=1097; i++)
+            {
+                if (i % 97 == 0)
+                {
+                    Console.WriteLine(i);
+                    break;
+                }
+            }
+            Console.ReadLine(); 
+        }
+    }
+}
