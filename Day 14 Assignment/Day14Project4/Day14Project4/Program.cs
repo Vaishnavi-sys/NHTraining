7f20b145e256387794d1b5893b68c2228da323fe
Vaishnavi-sys <vaishnavi.nanam@gmail.com>
2022-02-10 19:16:43 +0530

    prime number using break

---
+++ Day 14 Assignment/Day14Project4/Day14Project4/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day14Project4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int v , i;
+            Console.WriteLine("Enter a number: ");
+            v = Convert.ToInt32(Console.ReadLine());    
+            for(i=2;i<v;i++)
+            {
+                if (v % i == 0)
+                    break;
+            }
+
+            if (i == v)
+                Console.WriteLine("PRIME");
+            else
+                Console.WriteLine("NOT PRIME");
+
+            Console.ReadLine();
+        }
+    }
+}
