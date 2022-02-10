2e1a01ce88404e0882ab60fbcde984452c1556d2
Vaishnavi-sys <vaishnavi.nanam@gmail.com>
2022-02-10 17:45:35 +0530

    Sealed Class

---
+++ Day 14 Assignment/Day14Assignment1/Day14Assignment1/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day14Assignment1
+{
+    sealed 
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
