7d3172a61ff2c27ed64dea56c991935337376ede
Vaishnavi-sys <vaishnavi.nanam@gmail.com>
2022-02-10 22:13:35 +0530

    Normal Property Example code

---
+++ Day 14 Assignment/Day14Project_2/Day14Project_2/Program.cs
@@ -0,0 +1,65 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day14Project_2
+{
+    public class Employee
+    {
+        private int id;
+        public string name;
+        public int  salary;
+
+        public int Id
+        {
+            get
+            {
+                return id;
+            }
+            set
+            {
+                id = value;
+            }
+        }
+        public string Name
+        {
+            get
+            {
+                return name;
+            }
+            set
+            {
+                name = value;
+            }
+        }
+        public int Salary
+        {
+            get
+            {
+                return salary;
+            }
+            set
+            {
+                salary = value;
+            }
+        }
+    }
+    
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee emp = new Employee();
+            emp.Id = 501;
+            emp.Name = "Vaishnavi";
+            emp.Salary = 30000;
+            Console.Write($"id={emp.Id}, name={emp.Name}, salary= {emp.Salary}");
+
+            Console.ReadLine();
+        }
+        
+    }
+}
