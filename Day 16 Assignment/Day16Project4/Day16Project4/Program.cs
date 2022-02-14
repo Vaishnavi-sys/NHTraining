fdbbf0b345eaf499a4b62503d808e587ed9ec89c
Vaishnavi-sys <vaishnavi.nanam@gmail.com>
2022-02-14 20:44:20 +0530

    Day 14 Submission

--- Day 16 Assignment/Day16Project4/Day16Project4/Program.cs
+++ Day 16 Assignment/Day16Project4/Day16Project4/Program.cs
@@ -3,13 +3,53 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+// **************************************
+// Author : Nanam Vaishnavi
+// Purpose : Multiplication Table in object oriented way.
+// **************************************
 
 namespace Day16Project4
 {
+
+    class Multable
+    {
+        int n;
+        /// <summary>
+        /// Read input from user
+        /// </summary>
+        public void ReadData()
+        {
+            Console.WriteLine("Enter n: ");
+            n = Convert.ToInt32(Console.ReadLine());    
+        }
+        /// <summary>
+        /// GetFactorial
+        /// </summary>
+        public void GetMultiplication()
+        {
+            for (int i = 1; i <= 10; i++)
+            {
+                Console.WriteLine("{0}*{1}={2}", n, i, n * i);
+                
+            }
+           
+           
+        }
+
+    }
     internal class Program
     {
         static void Main(string[] args)
         {
+            Multable mult = new Multable();
+                mult.ReadData();
+                mult.GetMultiplication();
+
+                Console.ReadLine();
+
+            
+
+            
         }
     }
 }
---
+++ Day 16 Assignment/MyProject/ClientApp/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using VaishuLibrary;
+using PublicLibrary;
+// *******************************
+// Author : Nanam Vaishnavi
+// Purpose : Create a solution "MyProject" and Adding three projects
+// *******************************
+
+namespace ClientApp
+{
+    internal class Program
+    {
+        /// <summary>
+        /// Import 2 Class Libraries
+        /// </summary>
+      
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Mathematics.Factorial(5));
+            Console.WriteLine(Physics.FinalVelocity(5,8,2));
+
+            Mathematics.
+
+            
+        }
+    }
+}
