4d01d71e2cd984383d5777f2d75205d54150cff7
Vaishnavi-sys <vaishnavi.nanam@gmail.com>
2022-02-21 13:05:09 +0530

    Day 21 submission

---
+++ Day 21 Assignment/MyClient App1/MyClient App1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MyClient_App1.ServiceReference1;
+
+namespace MyClient_App1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            WebService1SoapClient ob = new WebService1SoapClient();
+            Console.WriteLine(ob.Factorial(5));
+            Console.WriteLine(ob.Add(5,8));
+            Console.WriteLine(ob.Mul(6,9));
+            Console.WriteLine(ob.Div(25,5));
+
+            Console.ReadLine();
+        }
+    }
+}
